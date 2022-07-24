966f8ae4bc75a09dd572469b7b2b3e58d03af719
kbrakendirci <kubrakendirci0@gmail.com>
2022-07-24 15:49:18 +0300

    Added Welcome

--- app/src/main/java/com/kotlinproject/ecommerceapp/WelcomeActivity.kt
+++ app/src/main/java/com/kotlinproject/ecommerceapp/WelcomeActivity.kt
@@ -3,6 +3,8 @@ package com.kotlinproject.ecommerceapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.animation.AnimationUtils
 import com.kotlinproject.ecommerceapp.databinding.ActivityWelcomeBinding
 import com.kotlinproject.ecommerceapp.databinding.FragmentWelcomeBinding
 import com.kotlinproject.ecommerceapp.fragment.onboarding.onBoardActivity
@@ -14,9 +16,34 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button7.setOnClickListener{
+        binding.button.setOnClickListener{
             var intent = Intent(applicationContext, onBoardActivity::class.java)
             startActivity(intent)
+
+        }
+        var yukaridanAsagi = AnimationUtils.loadAnimation(this,R.anim.yukaridan_asagi)
+        var asagidanyulari = AnimationUtils.loadAnimation(this,R.anim.asagidan_yukari)
+        var bulunduguyerdenAsagi = AnimationUtils.loadAnimation(this,R.anim.bulunanyerden_asagi)
+
+        binding.button.animation=yukaridanAsagi
+        binding.imageView3.animation=asagidanyulari
+        binding.button.setOnClickListener {
+
+            binding.imageView3.startAnimation(bulunduguyerdenAsagi)
+
+
+            //Animasyon bitmeden 1sn sonra MainActivity açılsın
+            object : CountDownTimer(1000,1000){
+                override fun onFinish() {
+                    var intent = Intent(applicationContext, onBoardActivity::class.java)
+                    startActivity(intent)
+                }
+
+                override fun onTick(p0: Long) {
+                    //işimiz yok burasıyla
+                }
+
+            }.start()
 
         }
     }
